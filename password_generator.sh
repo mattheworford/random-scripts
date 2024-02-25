736628107ad6fee9a944fb56290b4ba299efaae4
Matthew Orford <mattorford9@gmail.com>
2024-02-25 00:28:51 -0500

    add comment for shell scripts

--- password_generator.sh
+++ password_generator.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# Function to generate a random password
+# This script generates and prints a random password of a given length.
+# It prompts the user to enter the length of the password and then generates a
+# random string of the following characters: A-Z, a-z, 0-9, and the following
+# special characters: !@#$%^&*()_+{}[].
+# To run this script, make the file executable with 'chmod +x
+# password_generator.sh', and then run it with './password_generator.sh'.
+
 generate_password() {
   local length=$1
   LC_ALL=C tr -dc 'A-Za-z0-9!@#$%^&*()_+{}[]' </dev/urandom | head -c "$length"
 }
 
-# Prompt the user to enter the length of the password
 read -r -p "Enter the length of the password: " password_length
 
-# Call the function and store the generated password
 password=$(generate_password "$password_length")
 
-# Print the generated password
 echo "Generated password: $password"
--- temperature.sh
+++ temperature.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Check if osx-cpu-temp is installed
-if ! command -v osx-cpu-temp &> /dev/null; then
+# This script is designed to fetch the CPU temperature on a macOS system and output it in Fahrenheit.
+# To run this script, make the file executable with 'chmod +x temperature.sh', and then run it with './temperature.sh'.
+
+if ! command -v osx-cpu-temp &>/dev/null; then
     echo "osx-cpu-temp could not be found. Please install it using Homebrew with the command 'brew install osx-cpu-temp'."
     exit
 fi
 
-# Get the CPU temperature in Celsius
 TEMP_C=$(osx-cpu-temp | grep -o '[0-9]*\.[0-9]*')
 
-# Convert the temperature to Fahrenheit
 TEMP_F=$(echo "$TEMP_C * 9/5 + 32" | bc)
 
-echo $TEMP_F F
+echo "$TEMP_F" F
